715124d6eec29c4fa226bbc3948f3a0459e567cb
Freeze-2 <avdigitaldesigns@gmail.com>
2021-06-12 23:55:57 +0100

    Frog aligns to surface

--- Assets/Scripts/Yes/WormMovement.cs
+++ Assets/Scripts/Yes/WormMovement.cs
@@ -13,13 +13,23 @@ public class WormMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Time.timeScale = 0.5f;
         _rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {            // Rotates the body to align to the floor normal
+        if (Physics.Raycast(transform.position + (transform.forward * 0.2f), Vector3.down,
+            out RaycastHit hit, 10f, LayerMask.GetMask("Default")))
+        {
+            Physics.Raycast(transform.position - (transform.forward * 0.2f), Vector3.down,
+               out RaycastHit hit2, 10f, LayerMask.GetMask("Default"));
+
+            Quaternion newRot = Quaternion.FromToRotation
+                (transform.up, (hit.normal + hit2.normal).normalized) * transform.rotation;
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, newRot, 10f * Time.deltaTime);
+        }
     }
     private void FixedUpdate()
     {
@@ -27,7 +37,7 @@ public class WormMovement : MonoBehaviour
         {
             _rb.MoveRotation(Quaternion.Euler(Vector3.up * _torque * Input.GetAxis("Horizontal") * Time.fixedDeltaTime) * _rb.rotation);
         }
-        if (!Physics.Raycast(transform.position, Vector3.down, 3))
+        if (!Physics.Raycast(transform.position, Vector3.down, 1))
         {
             _rb.MovePosition(transform.position + (Vector3.up * Physics.gravity.y * 5f * Time.fixedDeltaTime));
         }
@@ -36,7 +46,7 @@ public class WormMovement : MonoBehaviour
     {
         Gizmos.DrawLine(_head.position, _head.position + Vector3.down * 5);
         Gizmos.color = Color.blue;
-        Gizmos.DrawLine(_head.position + Vector3.up * 10f, Vector3.down * 10f + _head.position);
-        Gizmos.DrawSphere(_head.position + Vector3.forward * 5 + Vector3.up * 10f, 0.1f);
+        Gizmos.DrawSphere(transform.position + (transform.forward * 0.2f), 0.1f);
+        Gizmos.DrawSphere(transform.position - (transform.forward * 0.2f), 0.1f);
     }
 }
