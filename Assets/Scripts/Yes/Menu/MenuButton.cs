88aeccc8defca0b6c5fcb7892b44226ff6bd9d4f
Freeze-2 <avdigitaldesigns@gmail.com>
2021-06-13 18:01:43 +0100

    Menu things

---
+++ Assets/Scripts/Yes/Menu/MenuButton.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using TMPro;
+
+public class MenuButton : MonoBehaviour
+{
+    [SerializeField] private UnityEvent _onJumpOnButton;
+    [SerializeField] private GarryController _garry;
+    private bool _entered;
+    private TextMeshPro _text;
+    private string _originalText;
+
+    private void Awake()
+    {
+        _text = GetComponentInChildren<TextMeshPro>();
+        _originalText = _text.text;
+
+        _garry.groundHit += TryActivation;
+    }
+
+    private void TryActivation(float maxHeight)
+    {
+        if (_entered)
+        {
+            _onJumpOnButton?.Invoke();
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        _entered = true;
+        _text.text = "> " + _originalText + " <";
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        _entered = false;
+        _text.text = _originalText;
+    }
+}
